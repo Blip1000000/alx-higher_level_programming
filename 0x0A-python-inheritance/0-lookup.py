ed0a55ee40a51d823dd13f77bee577147bf82717
Blip1000000 <Blip1000000@gmail.com>
2023-02-07 15:25:27 -0800

    ghfyt

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""  returns the list of available attributes
-and methods of an object
-"""
+"""lookup module"""
 
 
 def lookup(obj):
-    """returns the list of available attributes"""
+    """lookup method
+    Returns: a list of available attributes and methods of an object"""
     return dir(obj)
