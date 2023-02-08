c573f7808bfd385a47feefd7dc586ba14547b098
Blip1000000 <Blip1000000@gmail.com>
2023-02-07 23:42:39 -0800

    record

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,21 +6,3 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
-
-
-vi 1-write_file.py
-#!/usr/bin/python3
-"""Defines a file-writing function."""
-
-
-def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
